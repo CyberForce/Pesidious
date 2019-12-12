5f830db61f26b997d619a525c1b860ce73baf9ca
chandni vaya <vaya97chandni@gmail.com>
2019-12-12 14:26:22 +0400

    personalizer

--- personalizer.py
+++ personalizer.py
@@ -64,27 +64,6 @@ def get_actions():
 	action4 = RankableAction(id='imports_append', features=[{'name':'imports_append'}])
 	return [action1, action2, action3, action4]
 
-def get_actions1():
-    action1 = RankableAction(id='pasta', features=[{"taste":"salty", "spice_level":"medium"},{"nutrition_level":5,"cuisine":"italian"}])
-    action2 = RankableAction(id='ice cream', features=[{"taste":"sweet", "spice_level":"none"}, { "nutritional_level": 2 }])
-    action3 = RankableAction(id='juice', features=[{"taste":"sweet", 'spice_level':'none'}, {'nutritional_level': 5}, {'drink':True}])
-    action4 = RankableAction(id='salad', features=[{'taste':'salty', 'spice_level':'none'},{'nutritional_level': 2}])
-    return [action1, action2, action3, action4]
-
-def get_user_preference():
-    res = {}
-    taste_features = ['salty','sweet']
-    pref = input("What type of food would you prefer? Enter number 1.salty 2.sweet\n")
-    
-    try:
-        ppref = int(pref)
-        if(ppref<=0 or ppref>len(taste_features)):
-            raise IndexError
-        res['taste_preference'] = taste_features[ppref-1]
-    except (ValueError, IndexError):
-        print("Entered value is invalid. Setting feature value to", taste_features[0]+ ".")
-        res['taste_preference'] = taste_features[0]
-    return res
 
 if __name__ == "__main__":
 
@@ -111,7 +90,7 @@ if __name__ == "__main__":
 		#print(state_dict)
 	
 		for mutation in range(1, 80):
-			rank_request = RankRequest( actions=actions, context_features=state_dict, eventid=eventid)
+			rank_request = RankRequest( actions=actions, context_features=state_dict, event_id=eventid)
 			response = client.rank(rank_request=rank_request)
 
 			print("Personalizer service ranked the actions with the probabilities listed below:")
@@ -119,6 +98,8 @@ if __name__ == "__main__":
 			rankedList = response.ranking
 			for ranked in rankedList:
 			    print(ranked.id, ':',ranked.probability)
+
+		    client.events.reward(event_id=eventid, value=0)
 
 
 
