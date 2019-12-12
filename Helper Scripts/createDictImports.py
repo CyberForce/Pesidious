0833d7307a3d540594535cad60eed6ea9c9883f6
chandni vaya <vaya97chandni@gmail.com>
2019-12-12 19:10:00 +0400

    create pk

--- Helper Scripts/createDictImports.py
+++ Helper Scripts/createDictImports.py
@@ -7,6 +7,9 @@ infile = sys.argv[1]
 outputfile = sys.argv[2]
 outfile = open(outputfile, 'w')
 
+with open(infile, 'r') as imports:
+	imports.readlines()
+
 adversarial_imports_dict = {}
 dll = ""
 
@@ -21,3 +24,4 @@ for line in imports:
 for keys in adversarial_imports_dict:
 	for value in adversarial_imports_dict[key]:
 		outfile.write(key + ':' + value)
+
