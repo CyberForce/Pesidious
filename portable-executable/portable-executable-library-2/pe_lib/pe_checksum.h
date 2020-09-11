bfe70a722e236817d728a101b5b3dc7c45d27f5b
Bedang Sen <Bedang.Sen@ibm.com>
2020-09-11 15:48:46 +0000

    removed folder

--- portable-executable/portable-executable-library-2/pe_lib/pe_checksum.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <istream>
-#include "stdint_defs.h"
-
-namespace pe_bliss
-{
-//Calculate checksum of image (performs no checks on PE structures)
-uint32_t calculate_checksum(std::istream& file);
-}
